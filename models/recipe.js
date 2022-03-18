a65b8432008d68d4d6ed394c55a8efc26d3ae6de
John Webster <jwebster@valorem.com>
2022-03-18 13:59:36 -0700

    adding separate comment schema and timestamp for recipe

--- models/recipe.js
+++ models/recipe.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose');
 
+const commentSchema = new mongoose.Schema({
+    comment: String,
+    user: String,
+    userId: String
+}, { timestamps: true })
+
 const recipeSchema = new mongoose.Schema({
     user_id: String,
     r_name: String,
@@ -9,12 +15,8 @@ const recipeSchema = new mongoose.Schema({
     category: String,
     ingredients: Array,
     steps: Array,
-    comments: [{
-        comment: String,
-        user: String,
-        userId: String
-    }],
+    comments: [commentSchema],
     favorites: [String]
-});
+}, { timestamps: true });
 
 module.exports = mongoose.model('Recipe', recipeSchema);
